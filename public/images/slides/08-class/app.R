fd5dff1723bc154d5649d31742ac857c2e5a6d6e
Ryan Wesslen <rhymenoceros@Ryans-MacBook-Air.local>
2019-03-17 21:15:24 -0400

    text as data week: slides, lab, class

---
+++ public/images/slides/08-class/app.R
@@ -0,0 +1,40 @@
+# let's deploy this to shinyapps.io: click the blue button in the top right of this box
+
+# exercise 1: add a new slider that sets the number of breaks for the rnorm() function
+# exercise 2: add a textInput() that sets the name of the plot
+# exercise 3: add a actionButton() that updates the name of the plot (part 2) only when clicking; see ?observeEvent
+
+library(shiny)
+
+ui <- fluidPage(
+  
+  sidebarLayout(
+    
+    sidebarPanel(
+      sliderInput(inputId = "num",
+                  label = "Choose a number",
+                  value = 25, min = 1, max = 100),
+      sliderInput(inputId = "breaks",
+                  label = "Choose breaks",
+                  value = 25, min = 1, max = 100),
+      textInput(inputId = "hippo",
+                label = "Enter the title")
+    ),
+    
+    mainPanel(
+      plotOutput("hist")
+    )
+  ) 
+)
+
+server <- function(input, output) {
+  
+  output$hist <- renderPlot({
+    title <- "100 random normal values"
+    hist(rnorm(input$num), breaks = input$breaks, 
+         main = input$hippo)
+  })
+  
+}
+
+shinyApp(ui = ui, server = server)
